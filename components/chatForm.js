5434996fe97e2240adf988fd41ada73b7f6e1b3f
vplasencia <v.pcalana@gmail.com>
2022-08-23 01:15:45 -0400

    Change Chat form style

--- components/chatForm.js
+++ components/chatForm.js
@@ -1,14 +1,28 @@
-import styles from "../styles/ChatForm.module.css";
 import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 export default function ChatForm() {
+  const inputRef = useRef();
   const [phoneValue, setPhoneValue] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log(phoneValue);
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div className="flex justify-center items-center md:my-5 py-5">
-      <form className="space-y-10" noValidate>
+      <form
+        className="space-y-10"
+        noValidate
+        autoComplete="off"
+        onSubmit={sendMessage}
+      >
         <div className="grid space-y-2">
           <label htmlFor="phone" className="text-lg">
             Enter phone number
@@ -22,17 +36,19 @@ export default function ChatForm() {
             focus:outline-none
             bg-transparent
             border-cyan-600 focus:border-cyan-400"
-            autoComplete="off"
             id="phone"
             type="phone"
             name="phone"
           /> */}
-          <PhoneInput
-            placeholder="+12 3 4567890"
-            value={phoneValue}
-            onChange={setPhoneValue}
-            autoComplete="off"
-          />
+          <div>
+            <PhoneInput
+              placeholder="+12 3 4567890"
+              value={phoneValue}
+              onChange={setPhoneValue}
+              autoComplete="off"
+              ref={inputRef}
+            />
+          </div>
         </div>
         <div className="grid space-y-2">
           <label htmlFor="message" className="text-lg">
@@ -42,6 +58,7 @@ export default function ChatForm() {
             name="message"
             id="message"
             cols="50"
+            placeholder="Write a message..."
             className="
             h-48
             p-3
@@ -49,7 +66,7 @@ export default function ChatForm() {
             border
             rounded-lg
             focus:outline-none
-            border-cyan-600 focus:border-cyan-400
+            border-cyan-600
             bg-transparent
 
             "
