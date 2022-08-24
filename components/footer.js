940f97e09e6e301f04c94ea67be0231f162367d2
vplasencia <v.pcalana@gmail.com>
2022-08-23 23:25:38 -0400

    Add elements to the footer

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,29 @@
 import ViewSourceCode from "./viewSourceCode";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
-    <footer className="mt-10 mb-5 flex items-center justify-center">
-      <ViewSourceCode />
+    <footer className="mt-5 mb-5 flex items-center justify-center">
+      <div className="grid place-items-center">
+        <ViewSourceCode />
+        <div className="text-center py-3">
+          <span className="flex flex-wrap justify-center items-center space-x-1">
+            <span>{year}</span>
+            <span>&bull;</span>
+            <span>Made with</span>
+            <span>❤️</span>
+            <span>by</span>
+            <a
+              target="_blank"
+              rel="noreferrer noopener nofollow"
+              className="text-cyan-400 hover:underline"
+              href="https://twitter.com/ViviPlasenciaC"
+            >
+              Vivian Plasencia
+            </a>
+          </span>
+        </div>
+      </div>
     </footer>
   );
 }
--- components/header.js
+++ components/header.js
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between p-5 mb-5 md:mb-10">
+    <header className="flex flex-wrap justify-between items-center p-3 md:p-5 mb-5 md:mb-5">
       <Link href="/">
         <a className="text-2xl font-bold text-cyan-500">WhatsApp Chats</a>
       </Link>
       <Link href="/about">
-        <a className="text-cyan-400 hover:underline">About</a>
+        <a className="text-cyan-400 font-semibold px-5 py-3 rounded-lg hover:bg-slate-800 transition-all duration-150 ease-linear">About</a>
       </Link>
     </header>
   );
