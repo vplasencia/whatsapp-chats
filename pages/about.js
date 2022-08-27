349f1373b6a71ff8734f1ae21ea4e56e7670b370
vplasencia <v.pcalana@gmail.com>
2022-08-27 08:21:14 -0400

    Add About page text

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
 export default function About() {
   return (
-    <div>
-      <p>About page</p>
+    <div className="flex justify-center items-center mt-10 text-lg px-5">
+      <p className="w-auto sm:w-6/12">
+        WhatsApp Chats allows you to send WhatsApp messages to people without
+        having them in your contacts.
+      </p>
     </div>
   );
 }
